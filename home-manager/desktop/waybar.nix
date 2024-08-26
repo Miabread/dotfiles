71c194689f8101b7518189877076e567923b0893
Miabread <miabreadloaf@gmail.com>
2024-08-26 10:22:25 -0400

    Add more stats to waybar

--- home-manager/desktop/waybar.nix
+++ home-manager/desktop/waybar.nix
@@ -8,27 +8,52 @@
       mainBar = {
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "hyprland/window" ];
-        modules-right = [ "tray" "battery" "clock" ];
+        modules-right =
+          [ "network" "bluetooth" "pulseaudio" "backlight" "battery" "clock" ];
 
         "hyprland/workspaces" = {
-          "on-scroll-up" = "hyprctl dispatch workspace e+1";
-          "on-scroll-down" = "hyprctl dispatch workspace e-1";
+          on-scroll-up = "hyprctl dispatch workspace e+1";
+          on-scroll-down = "hyprctl dispatch workspace e-1";
 
-          "format" = "{icon}";
-          "format-icons" = {
+          format = "{icon}";
+          format-icons = {
             "1" = "";
             "2" = "";
             "3" = "";
             "4" = "";
             "5" = "";
           };
-          "persistent-workspaces"."*" = 5;
+          persistent-workspaces."*" = 5;
         };
 
         "hyprland/window" = {
-          "on-scroll-up" = "hyprctl dispatch workspace e+1";
-          "on-scroll-down" = "hyprctl dispatch workspace e-1";
+          on-scroll-up = "hyprctl dispatch workspace e+1";
+          on-scroll-down = "hyprctl dispatch workspace e-1";
         };
+
+        "network" = {
+          format = "{ifname}";
+          format-wifi = " {essid} ({signalStrength}%)";
+          format-ethernet = " {ipaddr}/{cidr}";
+        };
+
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-muted = "";
+          format-icons = { default = [ "" "" ]; };
+        };
+
+        "backlight" = {
+          format = "{icon} {percent}%";
+          format-icons = [ "" ];
+        };
+
+        "battery" = {
+          format = "{icon} {capacity}%";
+          format-icons = [ "" "" "" "" "" ];
+        };
+
+        "clock" = { format = " {:%H:%M}"; };
       };
     };
   };
