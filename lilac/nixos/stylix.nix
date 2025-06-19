3a0c2f2c78be8e466d3d99ee5178a4592a99a946
Miabread <miabreadloaf@gmail.com>
2025-06-19 17:19:43 -0400

    Change wallpaper and theme

--- lilac/nixos/stylix.nix
+++ lilac/nixos/stylix.nix
@@ -3,9 +3,30 @@
 {
   stylix = {
     enable = true;
-    image = ../wallpaper.png;
+    image = ../wallpaper2.png;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ocean.yaml";
+
+    base16Scheme = {
+      # Generated
+      base00 = "252724";
+      base01 = "464944";
+      base02 = "676b65";
+      base03 = "888d85";
+      base04 = "a9afa5";
+      base05 = "cad1c6";
+      base06 = "d1d7ce";
+      base07 = "d9ded7";
+
+      # Handpicked
+      base08 = "#F09F6E";
+      base09 = "#E5CBBA";
+      base0A = "#FEEBC3";
+      base0B = "#B9D2A8";
+      base0C = "#A7C4C2";
+      base0D = "#87B4D0";
+      base0E = "#97A5D3";
+      base0F = "#DAC0B1";
+    };
 
     fonts = {
       monospace = {
