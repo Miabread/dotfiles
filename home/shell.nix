4bc10f6e682eda80462459c4419a94d1a7e2a0a7
Miabread <miabreadloaf@gmail.com>
2025-07-15 00:45:44 -0400

    Try configuring nvf more

--- home/shell.nix
+++ home/shell.nix
@@ -25,14 +25,41 @@
     '';
   };
 
+  programs.hyfetch = {
+    enable = true;
+    settings = {
+      preset = "transgender";
+      mode = "rgb";
+      color_align = {
+        mode = "custom";
+        custom_colors = {
+          "1" = 1;
+          "2" = 0;
+        };
+      };
+    };
+  };
+
   programs.nvf = {
     enable = true;
+
     settings = {
       vim.viAlias = false;
       vim.vimAlias = true;
+
+      vim.extraPlugins = {
+        vscode = { 
+          package = pkgs.vimPlugins.vscode-nvim;
+          setup = "require('vscode').load('dark')";
+        };
+      };
+
+      # Status line
       vim.statusline.lualine.enable = true;
+      vim.statusline.lualine.setupOpts.options.theme = "vscode";
+      vim.options.showmode = false;
+
       vim.options = {
-        showmode = false;
         mouse = "";
       };
     };
