19f8368933a5fd3938ecff4f6f82704f792e53c1
Miabread <miabreadloaf@gmail.com>
2025-07-15 22:41:04 -0400

    Start syncthing

---
+++ nixos/syncthing.nix
@@ -0,0 +1,10 @@
+{ ... }:
+ 
+{
+  networking.firewall.allowedTCPPorts = [ 8384 ]; # TODO replace with nginx
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    guiAddress = "0.0.0.0:8384"; 
+  };
+}
